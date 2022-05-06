0582d24f31f3676d1c9158e12a575090ce6d1fc7
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-05-06 19:01:02 +0200

    feat: add version endpoint

---
+++ R.Systems.Tests/Controllers/VersionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace R.Systems.Tests.Controllers;
+
+[ApiController]
+[Route("version")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetVersion()
+    {
+        return Ok("1.0.0-preview.3");
+    }
+}
